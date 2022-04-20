0aacd104e2baa306927df688eead266ff5982482
Nithya <v.nithiya@gmail.com>
2022-04-20 16:40:55 +1000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StackOverflowPost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var post = new StackOverflow("Does my post work?", "Test to see if my post works.");
+            Console.WriteLine($"Title: {post.Title}");
+            Console.WriteLine($"Description: {post.Description}");
+            Console.WriteLine($"Date Created: {post.CreatedOn}");
+            Console.WriteLine($"Post Count: {post.VoteCount}");
+            post.VoteDown();
+            Console.WriteLine($"Post Count: {post.VoteCount}");
+            post.VoteUp();
+            Console.WriteLine($"Post Count: {post.VoteCount}");
+        }
+    }
+}
---
+++ StackOverflow.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflowPost
+{
+    public class StackOverflow
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedOn { get; } = DateTime.UtcNow;
+        public int VoteCount { get; private set; } = 0;
+
+        public StackOverflow(string title, string description)
+        {
+            if (string.IsNullOrEmpty(title))
+            {
+                throw new ArgumentNullException(nameof(title));
+            }
+            Title = title;
+            Description = description;
+        }
+
+        public void VoteUp()
+        {
+            switch (VoteCount)
+            {
+                case -1:
+                case 0:
+                    VoteCount += 1;
+                    break;
+            }
+        }
+
+        public void VoteDown()
+        {
+            switch (VoteCount)
+            {
+                case -1:
+                case 0:
+                    VoteCount -= 1;
+                    break;
+            }
+        }
+
+    }
+}
